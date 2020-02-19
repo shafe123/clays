71e61417f38c744bcb53a4905bf3d10c8528f83b
Ethan <ethan.h.shafer@gmail.com>
2020-02-19 12:52:59 -0500

    Template object tracking loop

--- VideoRecognition/VideoRecognition.py
+++ VideoRecognition/VideoRecognition.py
@@ -15,10 +15,8 @@ ORANGE_MAX = np.array([20, 255, 255], np.uint8)
 blob_params = cv2.SimpleBlobDetector_Params()
 blob_params.filterByArea = True
 blob_params.minArea = 100
-blob_params.maxArea = 100000000
-blob_params.filterByColor = False
-blob_params.minThreshold = 200
-blob_params.maxThreshold = 255
+blob_params.filterByColor = True
+blob_params.blobColor = 255
 blob_params.filterByInertia = False
 blob_detector = cv2.SimpleBlobDetector_create(blob_params)
 
@@ -28,12 +26,19 @@ top_left = (10, 10)
 fontScale = 1
 fontColor = (255,255,255)
 
+# keep track of how many frames in a row we matched
+frames_detected = 0
+
 while(1):
     len_wait = 1
     ret, fgmask = cap.read()
 
+    if not ret:
+        cap.release()
+        cv2.destroyAllWindows()
+        break
+
     #reduce frame window to limit processing
-    print(fgmask.shape)
     fgmask = fgmask[:, 960:, :]
 
     #convert to black/white
@@ -48,12 +53,21 @@ while(1):
 
     blob = blob_detector.detect(fgmask)
 
+    print(fgmask.shape)
     # if blobs detected
     if len(blob) > 0:
         to_draw = cv2.drawKeypoints(fgmask, blob, None, (255,255,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        # wait for keyboard input
         len_wait = -1
+
+        frames_detected += 1
     else:
         to_draw = cv2.putText(fgmask, 'No blob detected yet', top_left, font, fontScale, fontColor)
+        frames_detected == 0
+
+    if frames_detected > 3:
+        # switch over to object tracking
+        pass
 
     cv2.imshow('frame', to_draw)
     k = cv2.waitKey(len_wait) & 0xff
@@ -61,3 +75,4 @@ while(1):
         cap.release()
         cv2.destroyAllWindows()
         break
+
